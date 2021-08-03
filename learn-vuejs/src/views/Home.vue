c631b56b6281b1b62727b3fd732d20830f6f3eae
yoonyounghoon <yunyounghoon23@gmail.com>
2021-08-03 23:20:11 +0900

    라이프싸이클

--- learn-vuejs/src/views/Home.vue
+++ learn-vuejs/src/views/Home.vue
@@ -1,8 +1,43 @@
 <template>
   <div>
     <h1>Welcome to Home !</h1>
+    <input type="text" v-model="input" />
+    <button type="button" @click="getData">Get</button>
+    <button type="button" @click="setData">Set</button>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      input: 'abc',
+    };
+  },
+  methods: {
+    getData() {
+      console.log(this.input);
+    },
+    setData() {
+      this.input = '12345';
+    },
+  },
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+  created() {
+    console.log('created');
+  },
+  beforeMount() {
+    console.log('beforeMount');
+  },
+  mounted() {
+    console.log('mounted');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+  updated() {
+    console.log('updated');
+  },
+};
 </script>
