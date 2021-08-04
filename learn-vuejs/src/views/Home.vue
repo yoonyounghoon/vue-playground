4777d9d63a7be8490d9804d75fa34aa8aacf0721
yoonyounghoon <yunyounghoon23@gmail.com>
2021-08-04 13:28:00 +0900

    조건부 렌더링, 반복문

--- learn-vuejs/src/views/Home.vue
+++ learn-vuejs/src/views/Home.vue
@@ -4,14 +4,37 @@
     <input type="text" v-model="input" />
     <button type="button" @click="getData">Get</button>
     <button type="button" @click="setData">Set</button>
+    <img v-bind:src="imageSource" alt="random-image" />
+    <a :href="naverUrl">Naver</a>
+    <hr />
+    <h2 :class="{ red: input === 'apple', done: input === 'apple' }">
+      동적으로 클래스 주기
+    </h2>
+    <hr />
+    <input type="text" v-model="inputAge" />
+    <h2>당신의 나이는 {{ inputAge }}세입니다.</h2>
+    <h3 v-if="inputAge > 19">당신은 성인입니다.</h3>
+    <h3 v-else-if="inputAge > 13 && inputAge <= 19">당신은 청소년입니다.</h3>
+    <div>
+      <h2 v-for="(animal, index) in animals" :key="index">{{ animal }}</h2>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      input: 'abc',
+      input: '',
+      inputAge: '',
+      imageSource: 'https://placeimg.com/100/100/any',
+      naverUrl: 'https://www.naver.com',
+      animals: ['monkey', 'dog', 'cat'],
     };
+  },
+  watch: {
+    input() {
+      console.log(this.input);
+    },
   },
   methods: {
     getData() {
@@ -33,11 +56,20 @@ export default {
   mounted() {
     console.log('mounted');
   },
-  beforeUpdate() {
-    console.log('beforeUpdate');
-  },
-  updated() {
-    console.log('updated');
-  },
+  // beforeUpdate() {
+  //   console.log('beforeUpdate');
+  // },
+  // updated() {
+  //   console.log('updated');
+  // },
 };
 </script>
+<style>
+.red {
+  color: red;
+}
+
+.done {
+  text-decoration: line-through;
+}
+</style>
